c30a50e39db7227f6c8e3953831657deba872aab
MMMarz009 <mar0099887@gmail.com>
2020-07-21 20:36:42 -0500

    Completed orm & updated Schema

--- config/orm.js
+++ config/orm.js
@@ -3,14 +3,34 @@ const connection = require("./connection.js");
 
 const orm={
     //selectAll()
-
+    selectAll: function (cb){
+        const query = connection.query("SELECT * FROM burgers", function(err,res){
+            if (err) throw err;
+            cb(res);
+        })
+    },
 
 
 
     //insertOne()
+    insertOne: function(burgerName){
+        const query = connection.query("INSERT INTO burgers SET ?", {burger_name: burgerName}, function(err,res){
+            if (err) throw err;
+            console.log(res.affectedRows + "burger added. \n");
+        })
+
+    },
 
 
 
 
-    //updateOne()
+    //updateOne() update cuz devoured status
+    updateOne: function(burgerUpdate){
+        const query = connection.query("UPDATE burgers SET devoured = true WHERE ID = ?", { id: burgerUpdate}, function(err,res){
+            console.log(res.affectedRows + "Burger updated \n";)
+        })
+    }
 }
+
+
+module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,5 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(100),
-    devoured BOOLEAN
+    devoured BOOLEAN NOT NULL DEFAULT FALSE
 );
